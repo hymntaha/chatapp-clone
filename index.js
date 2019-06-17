3c8c4dfb87fe333b4658b09ee844bee79bbe1892
taha uygun <tahauygun@gmail.com>
2019-06-16 23:58:50 -0700

    adding graphql tools

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import { graphqlExpress } from 'apollo-server-express';
 
+import typeDefs from './schema';
+import resolvers from './resolvers'
+
+export const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers,
+});
+
+
 const app = express();
 app.use('/graphql', bodyParser.json(), graphqlExpress({ schema: myGraphQLSchema }));
 app.listen(8080);
